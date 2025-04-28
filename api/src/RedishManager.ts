e3f3bd8501c62a0197a605922b4071adccbfeb4e
amit kumar <mohapatraamitkumar450@gmail.com>
2025-04-28 13:18:32 +0530

    Added the RedisManeger class

--- api/src/RedishManager.ts
+++ api/src/RedishManager.ts
@@ -1,1 +1,39 @@
 import { RedisClientType, createClient } from "redis"
+import { MessageToEngine } from "./types/to";
+import { MessageFromOrderbook } from "./types";
+
+export class RedisManager {
+    private client: RedisClientType;
+    private publisher: RedisClientType;
+    private static instance: RedisManager;
+
+    private constructor() {
+        this.client = createClient();
+        this.publisher = createClient();
+        this.client.connect();
+        this.publisher.connect();
+    }
+
+    public static getInstance() {
+        if(!this.instance) {
+            this.instance = new RedisManager();
+        }
+
+        return this.instance;
+    }
+    // to avoid parallel transactions execution
+    public sendAndWait(message: MessageToEngine) {
+      return new Promise<MessageFromOrderbook>((resolve) => {
+        const id = this.getRandomClientId();
+        this.client.subscribe(id, (message) => {
+            this.client.unsubscribe(id);
+            resolve(JSON.parse(message))
+        });
+        this.publisher.lPush("message", JSON.stringify({clientId: id, message}));
+      })
+    } 
+
+    public getRandomClientId() {
+        return Math.random().toString(32).substring(2,15) + Math.random().toString(32).substring(2,15); 
+    }
+}
